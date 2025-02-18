c0b3a2ba537b9867a7aaad8f3c9a9d9605728c0f
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-02-18 17:00:29 +0900

    Initial Commit

---
+++ src/main/java/com/wheon/zulzulserver/ZulzulServerApplication.java
@@ -0,0 +1,13 @@
+package com.wheon.zulzulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZulzulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZulzulServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wheon/zulzulserver/ZulzulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheon.zulzulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZulzulServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
